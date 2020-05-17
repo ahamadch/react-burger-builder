48eec284139484f68838cf1281ed38e167565d61
Ahamad <ahamadch.officialAgmail.com>
2020-05-17 17:03:12 +0530

    Adding Sign-In

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -39,6 +39,7 @@ class Auth extends Component {
         touched: false
       }
     },
+    isSignUp: true
   }
 
   checkValidity(value, rules) {
@@ -91,7 +92,15 @@ class Auth extends Component {
 
   submitHandler = (event) => {
     event.preventDefault();
-    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+    this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignUp);
+  }
+
+  switchAuthModeHandler = () => {
+    this.setState(prevState => {
+      return {
+        isSignUp: !prevState.isSignUp
+      };
+    });
   }
 
   render() {
@@ -123,6 +132,11 @@ class Auth extends Component {
           {form}
           <Button btnType="Success" >SUBMIT</Button>
         </form>
+        <Button
+          btnType="Danger"
+          clicked={this.switchAuthModeHandler}>
+            SWITCH TO {this.state.isSignUp ? 'SIGNIN' : 'SIGNUP'}
+        </Button>
       </div>
     );
   }
@@ -130,7 +144,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
   return  {
-    onAuth: (email, password) => dispatch(actions.auth(email, password))
+    onAuth: (email, password, isSignUp) => dispatch(actions.auth(email, password, isSignUp))
   }
 }
 
