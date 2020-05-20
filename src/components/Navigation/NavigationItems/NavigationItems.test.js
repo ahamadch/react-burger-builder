9f1935951f2a067b017115b3586c9a4cdf43c8fb
Ahamad <ahamadch.officialAgmail.com>
2020-05-20 21:15:38 +0530

    Testing Components Correctly

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -23,4 +23,11 @@ describe('<NavigationItems />', () => {
     });
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it('should an exact logout button', () => {
+    wrapper.setProps({
+      isAuthenticated:  true
+    });
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+  });
 });
